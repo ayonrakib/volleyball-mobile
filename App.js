9b02e4fc49aadd57ad2654c785bf214de6a795ce
DESKTOP-A22BN7T\ayonr <ayonrakib@gmail.com>
2022-04-21 17:34:43 -0500

    first commit

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import { AppRegistry } from 'react-native';
+import { Provider as PaperProvider } from "react-native-paper";
 import HomeScreen from "./src/screens/HomeScreen";
 
-const navigator = createStackNavigator(
-  {
-    Home: HomeScreen,
-  },
-  {
-    initialRouteName: "Home",
-    defaultNavigationOptions: {
-      title: "App",
-    },
-  }
-);
+// const navigator = createStackNavigator(
+//   {
+//     Home: HomeScreen,
+//   },
+//   {
+//     initialRouteName: "Home",
+//     defaultNavigationOptions: {
+//       title: "App",
+//       headerShown: false
+//     },
+//   }
+// );
 
-export default createAppContainer(navigator);
+// export default createAppContainer(navigator);
+export default function App(){
+  return (
+    <PaperProvider>
+      <HomeScreen/>
+    </PaperProvider>
+  )
+}
+
+AppRegistry.registerComponent("Hello World", () => App)
