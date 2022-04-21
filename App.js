e7c5ab2e96ee829b878c0c4574853c884125107b
DESKTOP-A22BN7T\ayonr <ayonrakib@gmail.com>
2022-04-21 18:18:01 -0500

    login form

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { AppRegistry } from 'react-native';
 import { Provider as PaperProvider } from "react-native-paper";
+import { Provider as StoreProvider } from "react-redux";
 import HomeScreen from "./src/screens/HomeScreen";
+
 
 // const navigator = createStackNavigator(
 //   {
@@ -20,9 +22,10 @@ import HomeScreen from "./src/screens/HomeScreen";
 // export default createAppContainer(navigator);
 export default function App(){
   return (
-    <PaperProvider>
-      <HomeScreen/>
-    </PaperProvider>
+      <PaperProvider>
+        <HomeScreen/>
+      </PaperProvider>
+  
   )
 }
 
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,49 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
-
+import { View, Text, StyleSheet } from 'react-native';
+import { Button, TextInput } from 'react-native-paper';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+  return(
+        <View style={styles.form}>
+          <TextInput
+            mode='outlined'
+            style = {styles.inputStyle}
+            label="Email"
+            autoCapitalize='none'
+            outlineColor='gray'
+          />
+          <TextInput
+            mode='outlined'
+            style={styles.inputStyle}
+            label="Password"
+            autoCapitalize='none'
+            secureTextEntry={true}
+          />
+          <View style={{display: "flex", flexDirection: "row", justifyContent: "space-evenly", marginTop: 30}}>
+            <Button style={styles.buttonStyle} mode='contained'>Login</Button>
+            <Button style={styles.buttonStyle} mode='contained'>Register</Button>
+          </View>
+        </View>
+    ) 
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30,
-    marginTop: 30
+    marginTop: 120,
+    paddingTop: 50
   },
+  form:{
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center"
+  },
+  inputStyle:{
+    marginHorizontal: 50,
+    marginBottom: 15
+  },
+  buttonStyle:{
+    width: 122
+  }
 });
 
 export default HomeScreen;
